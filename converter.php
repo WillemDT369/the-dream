98abf8bc96c8765e153ee6ff762bd87c192f5791
WillemDT369 <detroyerwillem@hotmail.com>
2021-01-07 09:23:11 +0100

    Move php logic to top

--- converter.php
+++ converter.php
@@ -9,35 +9,33 @@
 </head>
 
 <body>
-    <div class="container">
-    <form class="converter-form" action="converter.php" method="POST">
-        <h2>Convert to Euro</h2>
-        <label for="price">Local Price:</label>
-        <input id="price" type="text" name="amount" placeholder="Enter Price"><br>
-        <label for="currency">Choose your currency:</label>
-        <select id="currency" type="text" name="dropdown">
-            <option value="XPF">XPF</option>
-            <option value="USD">USD</option>
-            <option value="GBP">GDP</option>
-        <input type="submit" name="convert" value="How much did I pay?">
-    </form>
     <?php
         if(isset($_POST['convert'])){
             $amount = $_POST['amount'];
             $dropdown = $_POST["dropdown"];
             if($dropdown == "XPF"){
                 $output = $amount * 0.00838;
-                echo "<h3>" ."€ ".$output  ."</h3>";
             } else if($dropdown == "USD"){
                 $output = $amount * 0.812015;
-                echo "<h3>" ."€ ".$output  ."</h3>";
             } else if($dropdown == "GBP"){
                 $output = $amount * 1.10259;
-                echo "<h3>" ."€ ".$output  ."</h3>";
             }
             
         } 
     ?>
+    <div class="container">
+        <form class="converter-form" action="converter.php" method="POST">
+            <h2>Convert to Euro</h2>
+            <label for="price">Local Price:</label>
+            <input id="price" type="text" name="amount" placeholder="Enter Price"><br>
+            <label for="currency">Choose your currency:</label>
+            <select id="currency" type="text" name="dropdown">
+                <option value="XPF">XPF</option>
+                <option value="USD">USD</option>
+                <option value="GBP">GDP</option>
+            <input type="submit" name="convert" value="How much did I pay?">
+        </form>
+        <h3>€ <?= $output; ?></h3>
     </div>
 </body>
 
